4169f007b63151d80686f22b9dec64106279fd72
Joseph Lee <developerjosephlee97@gmail.com>
2023-06-04 21:27:46 +0900

    first commit

---
+++ src/main/kotlin/com/lysanghwa/lessoncontent/LessonContentApplication.kt
@@ -0,0 +1,11 @@
+package com.lysanghwa.lessoncontent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LessonContentApplication
+
+fun main(args: Array<String>) {
+    runApplication<LessonContentApplication>(*args)
+}
---
+++ src/test/kotlin/com/lysanghwa/lessoncontent/LessonContentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lysanghwa.lessoncontent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LessonContentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
